cfad777ad38ea134833d6a6876514ffc679cae56
pavjelv <32219921+pavjelv@users.noreply.github.com>
2018-10-28 13:35:03 +0300

    Add files via upload

--- 1606-1/ParallelProgramming.cpp
+++ 1606-1/ParallelProgramming.cpp
@@ -9,111 +9,111 @@
 using namespace std;
 
 
-int main(int argc, char *argv [])
+int main(int argc, char *argv[])
 {
-	
-	MPI_Init(&argc,&argv);
+
+	MPI_Init(&argc, &argv);
 	int worldSize;
 	MPI_Comm_size(MPI_COMM_WORLD, &worldSize);
 	int rank;
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+	int* data;
+	int* minsArr;
+	int cols = 0;
+	int rows= 0;
+	double startTime = 0;
+	double endTime = 0;
 	if (rank == 0) {
-		int col = 0;
-		int row = 0;
+		int min = 0;
+		int max = 0;
 		cout << "Please, enter number of rows ";
-		cin >> row;
+		cin >> rows;
 		cout << endl << "Please, enter number of columns ";
-		cin >> col;
+		cin >> cols;
+		cout << endl << "Please, enter min: ";
+		cin >> min;
+		cout << endl << "Please, enter max: ";
+		cin >> max;
 		cout << endl;
-		int size[] = {row,col};
-		MPI_Bcast(size, 2, MPI_INT, 0, MPI_COMM_WORLD);
+		startTime = MPI_Wtime();
 		srand(time(nullptr));
-		int ** data = new int *[row];
-		for (int i = 0; i < row; i++) {
-			data[i] = new int[col];
-			for (int j = 0; j < col; j++) {
-				data[i][j] = rand() % 100;
-				cout << data[i][j] << " ";
+		int sizeArr = rows*cols;
+		cout << endl << sizeArr << endl;
+		data = new int[sizeArr];
+		for (int i = 0; i < sizeArr; i++) {
+			data[i] = min + rand() % (max - min + 1);
+		}
+		
+		for (int j = 0; j < rows; j++) {
+			for (int i = 0; i < cols; i++) {
+				cout << data[i * rows + j] << " ";
 			}
 			cout << endl;
 		}
-		
 
-		if (worldSize != 1) {
-			int min = data[0][0];
-			cout << min << " ";
-			for (int j = 1; j < row; j++) {
-				cout << data[j][0] << " ";
-				if (data[j][0] < min) min = data[j][0];
-			}
-			cout << " min element is " << min << endl;
-			
-			for (int k = 0; k < col / (worldSize - 1); k++) {
-				int thread = 1;
-				for (int i = 1; i < worldSize; i++) {
-					int* request = new int[row];
-					for (int j = 0; j < row; j++) {
-						request[j] = data[j][i + (worldSize - 1) * k];
-					}
-					MPI_Send(request, row, MPI_INT, thread, 0, MPI_COMM_WORLD);
-					thread++;
-				}
-			}
-			int s = 1;
-			for (int i = (col / (worldSize - 1)) * (worldSize - 1); i < col; i++) {
-				int* request = new int[row];
-				for (int j = 0; j < row; j++) {
-					request[j] = data[j][i];
-				}
-				MPI_Send(request, row, MPI_INT, s, 0, MPI_COMM_WORLD);
-				s++;
-			}
-			for (int i = 1; i < col; i++) {
-				int *resp1 = new int[row + 1];
-				MPI_Recv(resp1, row + 1, MPI_INT, MPI_ANY_SOURCE, 0, MPI_COMM_WORLD, MPI_STATUSES_IGNORE);
-				for (int j = 0; j < row; j++) {
-					cout << resp1[j] << " ";
-				}
-				cout << "  min element is " << resp1[row] << endl;
-			}
-		}
-
+		minsArr = new int[cols];
 	}
-	else {
-		int size [2];
+		int size[] = { rows,cols };
 		MPI_Bcast(size, 2, MPI_INT, 0, MPI_COMM_WORLD);
 		int row = size[0];
 		int col = size[1];
-		int* resp = new int[row];
-		for (int i = 0; i < col / (worldSize - 1); i++) {
-			MPI_Recv(resp, row, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUSES_IGNORE);
-			int minElem = resp[0];
-			int * answer = new int[row + 1];
-			answer[0] = minElem;
-			for (int j = 1; j < row; j++) {
-				answer[j] = resp[j];
-				if (minElem > resp[j])
-					minElem = resp[j];
-			}
-			answer[row] = minElem;
-			MPI_Send(answer, row + 1, MPI_INT, 0, 0, MPI_COMM_WORLD);
+		
+		int * offset = new int[worldSize];
+		int* sendCounts = new int[worldSize];
+		for (int i = 0; i < worldSize; i++) {
+			sendCounts[i] = (col / worldSize) * row;
 		}
-		if (rank <= col % (worldSize - 1)) {
-			int * resp2 = new int[row];
-			MPI_Recv(resp2, row, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUSES_IGNORE);
-			int minElem = resp[0];
-			int * resp3 = new int[row + 1];
-			resp3[0] = minElem;
-			for (int j = 1; j < row; j++) {
-				resp3[j] = resp[j];
-				if (minElem > resp[j])
-					minElem = resp[j];
-			}
-			resp3[row] = minElem;			
-			MPI_Send(resp3, row + 1, MPI_INT, 0, 0, MPI_COMM_WORLD);
+		for (int i = 0; i < col % worldSize; i++) {
+			sendCounts[i] += row;
 		}
-	}
+
+		int displ = 0;
+		for (int i = 0; i < worldSize; i++) {
+			offset[i] = displ;
+			displ += sendCounts[i];
+		}
+
+		int * recBuf = new int[sendCounts[rank]];
+
+		MPI_Scatterv(data, sendCounts, offset , MPI_INT, recBuf, sendCounts[rank], MPI_INT, 0, MPI_COMM_WORLD);
+		
+		int * sendBuf = new int[sendCounts[rank] / row];
+		int j = 0;
+		int r = 1;
+		int min = recBuf[0];
+		for (int i = 0; i < sendCounts[rank]; i++) {
+			if (min > recBuf[i]) min = recBuf[i];
+			if (r == row) {
+				sendBuf[j] = min;
+				j++;
+				min = recBuf[i + 1];
+				r = 0;
+			}
+			r++;
+		}
+		
+		int * offset1 = new int[worldSize];
+		int d = 0;
+		int * recvCounts = new int[worldSize];
+		for (int i = 0; i < worldSize; i++) {
+			recvCounts[i] = sendCounts[i] / row;
+			offset1[i] = d;
+			d += recvCounts[i];
+		}
+
+		MPI_Gatherv(sendBuf, sendCounts[rank] / row, MPI_INT, minsArr, recvCounts, offset1, MPI_INT, 0, MPI_COMM_WORLD);
+
+		if (rank == 0) {
+			cout << endl;
+			for (int i = 0; i < cols; i++) {
+				cout << "Min Element for " << i << " row is: " << minsArr[i] << endl;
+			}
+			endTime = MPI_Wtime();
+			cout << "Execution time is: " << endTime - startTime << endl;
+			delete[] minsArr, data, offset, offset1, sendBuf, sendCounts, recvCounts, recBuf;
+		}
 	MPI_Finalize();
+	
 	return 0;
 }
 
